4969d06095004432f818e3d4014a13e3caabfdba
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-04-10 12:18:24 -0600

    Set up project with dummy content

---
+++ app.js
@@ -0,0 +1,34 @@
+let library = [
+    {
+        title: 'The Lord of the Rings',
+        author: 'J.R.R. Tolkien',
+        year: 1954,
+        read: true,
+    },
+
+    {     
+        title: 'The Giver',
+        author: 'Lois Lowry',
+        year: 1993,
+        read: true,
+    },
+
+    {
+        title: 'Dune',
+        author: 'Frank Herbert',
+        year: 1965,
+        read: false,
+    },
+];
+
+function Book(title, author, year, read) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.read = read;
+}
+
+function addBook(title, author, year, read) {
+  library.push(new Book(title, author, year, read));
+}
+
