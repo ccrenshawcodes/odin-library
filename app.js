ad8863840740e407d87df724492b9dc30153549e
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-04-11 16:52:20 -0600

    Nest inputs in form element
    Nested inputs inside a form element so I could actually access
    the text inside them.
    
    Now the "add" button creates a new card with mostly correct info

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const container = document.querySelector('.container');
-const addBtn = document.querySelector('.open');
+const openModal = document.querySelector('.open');
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close');
+const addBtn = document.querySelector('.add');
+
+let bkTitle = document.querySelector('#title');
+let bkAuthor = document.querySelector('#author');
+let bkYear = document.querySelector('#year');
+let bkRead = document.querySelector('#read');
+
 
 let library = [
     {
@@ -33,21 +40,37 @@ function Book(title, author, year, read) {
     this.read = read;
 }
 
+//add an object to the library array
 function addBook(title, author, year, read) {
   library.push(new Book(title, author, year, read));
 }
 
-library.forEach(item => {
-    const card = document.createElement('div');
-    card.append(`Title: ${item.title}; Author: ${item.author}; Year: ${item.year}; Read? ${item.read}`);
-    container.appendChild(card);
-})
+//display array items as cards
+function displayItems() {
+    library.forEach(item => {
+        const card = document.createElement('div');
+        card.append(`Title: ${item.title}; Author: ${item.author}; Year: ${item.year}; Read? ${item.read}`);
+        container.appendChild(card);
+    })
+}
+displayItems();
 
-addBtn.addEventListener('click', () => {
+
+//modal to appear when "add a new book" is clicked
+openModal.addEventListener('click', () => {
     modal.style.display = 'inline-block';
 });
 
+//modal to be hidden when X is clicked
 closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
 })
 
+
+
+//form data to be added to the array when "add" is clicked
+addBtn.addEventListener('click', () => {
+    addBook(bkTitle.value, bkAuthor.value, bkYear.value, bkRead.value);
+    modal.style.display = 'none';
+    displayItems();
+});
